9f8c6f2d75f240e3eff0b2285da299da96b4e209
Drinnn <pedro_lontro@hotmail.com>
2023-10-29 15:02:08 -0300

    feat: add get all products

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -33,6 +33,7 @@ func main() {
 
 	r.Post("/products", productHandler.CreateProduct)
 	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Get("/products", productHandler.GetProducts)
 	r.Put("/products/{id}", productHandler.UpdateProduct)
 	r.Delete("/products/{id}", productHandler.DeleteProduct)
 	http.ListenAndServe(":3000", r)
--- internal/infra/webserver/handlers/product_handlers.go
+++ internal/infra/webserver/handlers/product_handlers.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/Drinnn/go-expert-api/internal/dto"
 	"github.com/Drinnn/go-expert-api/internal/entity"
@@ -61,6 +62,34 @@ func (h *ProductHandler) GetProduct(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 
 	json.NewEncoder(w).Encode(product)
+}
+
+func (h *ProductHandler) GetProducts(w http.ResponseWriter, req *http.Request) {
+	page := req.URL.Query().Get("page")
+	limit := req.URL.Query().Get("limit")
+	sort := req.URL.Query().Get("sort")
+
+	pageInt, err := strconv.Atoi(page)
+	if err != nil {
+		pageInt = 0
+	}
+	limitInt, err := strconv.Atoi(limit)
+	if err != nil {
+		limitInt = 0
+	}
+
+	products, err := h.ProductDB.FindAll(pageInt, limitInt, sort)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(products)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 func (h *ProductHandler) UpdateProduct(w http.ResponseWriter, req *http.Request) {
